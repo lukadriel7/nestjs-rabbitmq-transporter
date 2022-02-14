bcfd7d856da0f3b63f0a71e1b9d546813328e1a6
Yozan Bi Adriel junior <lukadriel@gmail.com>
2022-02-14 21:45:57 +0000

    add new events and messages

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,11 @@
-export const NO_MESSAGE_HANDLER = `There is no matching message handler defined in the remote service.`;
+export const NO_MESSAGE_HANDLER =
+  'There is no matching message handler defined in the remote service.';
 export const CONNECT_EVENT = 'connect';
+export const CONNECTED_RMQ_MESSAGE = 'Connected to RMQ Server';
 export const DISCONNECT_EVENT = 'disconnect';
-export const DISCONNECTED_RMQ_MESSAGE = `Disconnected from RMQ. Trying to reconnect.`;
+export const DISCONNECTED_RMQ_MESSAGE =
+  'Disconnected from RMQ. Trying to reconnect.';
 export const ERROR_EVENT = 'error';
+export const ERROR_RMQ_MESSAGE = 'An error occured';
+export const CONNECT_FAILED_EVENT = 'connectFailed';
+export const CONNECT_FAILED_RMQ_MESSAGE = 'The connection to the server failed';
